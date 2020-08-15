cabd5219840e41bd2079a25f4ee7fa4de422de56
ITkah <ilatkacenko132@gmail.com>
2020-08-16 00:33:10 +0300

    first-download

---
+++ js/common.js
@@ -0,0 +1,7 @@
+// var show_modal_ojag = document.getElementById('show_modal_ojag');
+// var modal_ojag = document.getElementById('ojag_modal');
+// var close_modal = document.getElementById('close_modal');
+
+// show_modal_ojag.onclick = function() {
+//     modal_ojag.classList.add("active_modal");
+// };
