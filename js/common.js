1dd04dc361bdf5f21f04704d7aeb8d603ac39d82
ITkah <ilatkacenko132@gmail.com>
2020-08-19 15:10:09 +0300

    a3 modal

--- js/common.js
+++ js/common.js
@@ -1,7 +1,78 @@
-// var show_modal_ojag = document.getElementById('show_modal_ojag');
-// var modal_ojag = document.getElementById('ojag_modal');
-// var close_modal = document.getElementById('close_modal');
+$('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+    $(".slider").slick('refresh');
+    $(this)
+      .addClass('active').siblings().removeClass('active')
+      .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+});
 
-// show_modal_ojag.onclick = function() {
-//     modal_ojag.classList.add("active_modal");
-// };
+$('.slider-for').on(`init reInit`, function(event, slick) {
+  $('.counter').text(1 + ' из ' + slick.slideCount);
+});
+$('.slider-for').on(`afterChange`, function(event, slick, currentSlide, nextSlide) {
+  $('.counter').text(currentSlide + 1 + ' из ' + slick.slideCount);
+});
+
+$('.slider-for-2').on(`init reInit`, function(event, slick) {
+  $('.counter').text(1 + ' из ' + slick.slideCount);
+});
+$('.slider-for-2').on(`afterChange`, function(event, slick, currentSlide, nextSlide) {
+  $('.counter').text(currentSlide + 1 + ' из ' + slick.slideCount);
+});
+
+$(".dtp_left").on("click", function(){
+  $(".slider-for .slick-prev").click();
+});
+
+$(".dtp_right").on("click", function(){
+  $(".slider-for .slick-next").click();
+});
+
+$(".zona_left").on("click", function(){
+  $(".slider-for-2 .slick-prev").click();
+});
+
+$(".zona_right").on("click", function(){
+  $(".slider-for-2 .slick-next").click();
+});
+
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: false,
+  arrows: true,
+  fade: true,
+  speed: 600,
+  asNavFor: '.slider-nav'
+});
+
+$('.slider-nav').slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: false,
+  arrows: false,
+  focusOnSelect: true,
+  centerMode: true,
+  centerPadding: '90px',
+});
+
+$('.slider-for-2').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: false,
+  arrows: true,
+  fade: true,
+  speed: 600,
+  asNavFor: '.slider-nav-2'
+});
+
+$('.slider-nav-2').slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for-2',
+  dots: false,
+  arrows: false,
+  focusOnSelect: true,
+  centerMode: true,
+  centerPadding: '90px',
+});
