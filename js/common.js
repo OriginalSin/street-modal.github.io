14792ae6857a1bf8074f684563c7a74206de30bd
ITkah <ilatkacenko132@gmail.com>
2020-08-27 14:49:57 +0300

    add-slider

--- js/common.js
+++ js/common.js
@@ -1,41 +1,41 @@
 $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
-    $(".slider").slick('refresh');
+    $(".modal_dtp .slider").slick('refresh');
     $(this)
       .addClass('active').siblings().removeClass('active')
       .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
 });
 
-$('.slider-for').on(`init reInit`, function(event, slick) {
+$('.modal_dtp .slider-for').on(`init reInit`, function(event, slick) {
   $('.counter').text(1 + ' из ' + slick.slideCount);
 });
-$('.slider-for').on(`afterChange`, function(event, slick, currentSlide, nextSlide) {
+$('.modal_dtp .slider-for').on(`afterChange`, function(event, slick, currentSlide, nextSlide) {
   $('.counter').text(currentSlide + 1 + ' из ' + slick.slideCount);
 });
 
-$('.slider-for-2').on(`init reInit`, function(event, slick) {
+$('.modal_dtp .slider-for-2').on(`init reInit`, function(event, slick) {
   $('.counter').text(1 + ' из ' + slick.slideCount);
 });
-$('.slider-for-2').on(`afterChange`, function(event, slick, currentSlide, nextSlide) {
+$('.modal_dtp .slider-for-2').on(`afterChange`, function(event, slick, currentSlide, nextSlide) {
   $('.counter').text(currentSlide + 1 + ' из ' + slick.slideCount);
 });
 
-$(".dtp_left").on("click", function(){
+$(".modal_dtp .dtp_left").on("click", function(){
   $(".slider-for .slick-prev").click();
 });
 
-$(".dtp_right").on("click", function(){
+$(".modal_dtp .dtp_right").on("click", function(){
   $(".slider-for .slick-next").click();
 });
 
-$(".zona_left").on("click", function(){
+$(".modal_dtp .zona_left").on("click", function(){
   $(".slider-for-2 .slick-prev").click();
 });
 
-$(".zona_right").on("click", function(){
+$(".modal_dtp .zona_right").on("click", function(){
   $(".slider-for-2 .slick-next").click();
 });
 
-$('.slider-for').slick({
+$('.modal_dtp .slider-for').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   dots: false,
@@ -45,7 +45,7 @@ $('.slider-for').slick({
   asNavFor: '.slider-nav'
 });
 
-$('.slider-nav').slick({
+$('.modal_dtp .slider-nav').slick({
   slidesToShow: 2,
   slidesToScroll: 1,
   asNavFor: '.slider-for',
@@ -56,7 +56,7 @@ $('.slider-nav').slick({
   centerPadding: '90px',
 });
 
-$('.slider-for-2').slick({
+$('.modal_dtp .slider-for-2').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   dots: false,
@@ -66,7 +66,7 @@ $('.slider-for-2').slick({
   asNavFor: '.slider-nav-2'
 });
 
-$('.slider-nav-2').slick({
+$('.modal_dtp .slider-nav-2').slick({
   slidesToShow: 2,
   slidesToScroll: 1,
   asNavFor: '.slider-for-2',
@@ -75,4 +75,55 @@ $('.slider-nav-2').slick({
   focusOnSelect: true,
   centerMode: true,
   centerPadding: '90px',
+});
+
+
+$('.modal_complex .slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: false,
+  arrows: true,
+  fade: true,
+  speed: 600,
+  asNavFor: '.slider-nav'
+});
+
+$('.modal_complex .slider-nav').slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: false,
+  arrows: false,
+  focusOnSelect: true,
+  centerMode: true,
+  centerPadding: '90px',
+});
+
+
+$(".cart_watch").on("click", function(e){
+  e.preventDefault();
+  $(".back_slider").show();
+  $(".complex_basis_main").hide();
+  $(".complex_slider_wrap").show();
+  $(".modal_complex .slider").slick('refresh');
+  $(".modal_complex .slider-for").on(`init reInit`, function(event, slick) {
+    $('.counter').text(1 + ' из ' + slick.slideCount);
+  });
+  $(".modal_complex .slider-for").on(`afterChange`, function(event, slick, currentSlide, nextSlide) {
+    $('.counter').text(currentSlide + 1 + ' из ' + slick.slideCount);
+  });
+});
+
+$(".back_slider").on("click", function(){
+  $(".back_slider").hide();
+  $(".complex_basis_main").show();
+  $(".complex_slider_wrap").hide();
+});
+
+$(".modal_complex .slider_right").on("click", function(){
+  $(".slider-for .slick-prev").click();
+});
+
+$(".modal_complex .slider_left").on("click", function(){
+  $(".slider-for .slick-next").click();
 });
